96a2083ee152167124234731c3b6159a32f807df
macharlasrilakshmi <101548823+macharlasrilakshmi@users.noreply.github.com>
2022-03-15 02:36:54 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,3 @@
+# accept a string and count the frequency of each vowel.
+Userinput = input("Enter a sentence: ")
+print(Userinput.count('a')+Userinput.count('e')+Userinput.count('i')+Userinput.count('o')+Userinput.count('u'))
