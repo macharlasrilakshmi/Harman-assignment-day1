a8c1d0588f5e9bbeac4a05d499c0482cb11b7fe7
macharlasrilakshmi <101548823+macharlasrilakshmi@users.noreply.github.com>
2022-03-15 02:40:17 +0530

    Create program5.py

---
+++ program5.py
@@ -0,0 +1,4 @@
+# To display the smallest and largest word from the string.
+Userinput = input("Enter a sentence")
+print("Smallest= ", min(Userinput).upper())
+print("Largest= ", max(Userinput).upper())
